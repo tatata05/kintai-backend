116bbd51b5b552ffecf49c73e6fd71a75f4f976b
tatata05 <takumi.takahata@xincere.jp>
2023-10-22 19:25:17 +0900

    create_db

---
+++ db/migrate/20230330050113_create_admins.rb
@@ -0,0 +1,11 @@
+class CreateAdmins < ActiveRecord::Migration[6.1]
+  def change
+    create_table :admins do |t|
+      t.string :name, null: false
+      t.string :email, null: false
+
+      t.timestamps
+    end
+    add_index :admins, :email, unique: true
+  end
+end
