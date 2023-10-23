5e6ea6925a11bf5fb88a68f950e9d59d06e4a0ff
tatata05 <takumi.takahata@xincere.jp>
2023-10-23 22:39:36 +0900

    check_scaffold

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,5 +1,6 @@
-class AdminsController < ApplicationController
+class Api::V1::AdminsController < ApplicationController
   def show
     @admin = Admin.find(params[:id])
+    render json: @admin
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      resources :books
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :admins
+  namespace :api do
+    namespace :v1 do
+      resources :admins
+    end
+  end
 end
