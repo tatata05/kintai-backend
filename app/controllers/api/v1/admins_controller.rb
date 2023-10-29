87ff63e83e75a4e37cadaa578900fa095ad552ce
tatata05 <takumi.takahata@xincere.jp>
2023-10-29 13:26:46 +0900

    comment

--- app/controllers/api/v1/admins_controller.rb
+++ app/controllers/api/v1/admins_controller.rb
@@ -4,6 +4,7 @@ class Api::V1::AdminsController < ApplicationController
   end
 
   def current_admin
+    # TODO: ログインをすると、headerに含まれている access-token, client, uid, の情報が必要。上記3つをsession? に保存しておく???
     @admin = current_api_v1_admin
   end
 end
--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,9 +1,12 @@
 class Admin < ApplicationRecord
   # Include default devise modules.
+  # omniauth はSNS認証機能作成時に必要。
+  # trackabele はログイン回数などの情報を保持、更新するのに必要らしい
   # :omniauthable, :confirmable :trackable
   devise :database_authenticatable, :registerable,
           :recoverable, :rememberable, :validatable
   include DeviseTokenAuth::Concerns::User
 
+  # deviseでpassword_confimationが適切に動かないため、自作。そもそもdeviseにconfimationは無いのか?
   validates :password, confirmation: true
 end
