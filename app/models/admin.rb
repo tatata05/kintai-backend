bd149c07a145e14d3b6da3666b6b7f84a58505ad
tatata05 <takumi.takahata@xincere.jp>
2023-10-29 12:00:31 +0900

    response_ok

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,7 +1,7 @@
 class Admin < ApplicationRecord
   # Include default devise modules.
+  # :omniauthable, :confirmable
   devise :database_authenticatable, :registerable,
-          :recoverable, :rememberable, :trackable, :validatable,
-          :confirmable, :omniauthable
+          :recoverable, :rememberable, :trackable, :validatable
   include DeviseTokenAuth::Concerns::User
 end
