288ed305056ec1e082aa2193c16637b530ff67a3
tatata05 <takumi.takahata@xincere.jp>
2023-11-07 18:09:09 +0900

    change_path

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api, defaults: {format: "json"} do
     namespace :v1 do
-      resources :admins do
-        collection do
-          get :current_admin
+      
+      namespace :admin do
+        resources :admins do
+          collection do
+            get :current_admin
+          end
         end
       end
 
