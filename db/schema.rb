b9f3a9fa6f8cefbe81ab94dacc0e24a17ce210af
tatata05 <takumi.takahata@xincere.jp>
2023-11-07 17:46:17 +0900

    fix: migration_date

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_10_26_104303) do
+ActiveRecord::Schema.define(version: 2023_11_06_064804) do
 
   create_table "absences", force: :cascade do |t|
     t.integer "status", default: 0
