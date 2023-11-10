e4fcc01e064bf6ba00b06396b0e35edfc4a54cd2
tatata05 <takumi.takahata@xincere.jp>
2023-11-10 10:26:29 +0900

    notification_update_response

--- app/controllers/api/v1/admin/notifications_controller.rb
+++ app/controllers/api/v1/admin/notifications_controller.rb
@@ -6,15 +6,9 @@ class Api::V1::Admin::NotificationsController < ApplicationController
     @notifications = notifications.by_recently_created.page(params[:page])
   end
 
-  # def update
-  #   notification = Notification.find_by(id: params[:id])
-  #   notification.update_attribute(:read, true) if notification.read == false
-  #   if notification.shift_id.present?
-  #     redirect_to admin_shift_path(notification.shift_id)
-  #   elsif notification.absence_id.present?
-  #     redirect_to admin_absence_path(notification.absence_id)
-  #   else
-  #     redirect_to admin_employee_path(notification.employee_id)
-  #   end
-  # end
+  def update
+    notification = Notification.find_by(id: params[:id])
+    notification.update(read: true) if notification.read == false
+    render status: 204, json: "success"
+  end
 end
