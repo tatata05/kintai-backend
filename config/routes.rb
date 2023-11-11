1886f0592326778aaeff5c6f972941f267fbfd86
tatata05 <takumi.takahata@xincere.jp>
2023-11-11 17:45:04 +0900

    can_use_devise_method

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  mount_devise_token_auth_for 'Admin', at: 'api/v1/admin_auth', controllers: {
+    registrations: "api/v1/admin_auth/registrations"
+  }
+  
+  mount_devise_token_auth_for 'Employee', at: 'api/v1/employee_auth'
+
   namespace :api, defaults: {format: "json"} do
     namespace :v1 do
       namespace :admin do
@@ -12,15 +18,6 @@ Rails.application.routes.draw do
         resources :employees, only: %i[index show]
         resources :notifications, only: %i[index update]
         resources :shifts, only: %i[index show update]
-      end
-
-      mount_devise_token_auth_for 'Admin', at: 'admin_auth', controllers: {
-        registrations: "api/v1/admin_auth/registrations"
-      }
-
-      mount_devise_token_auth_for 'Employee', at: 'employee_auth'
-      as :employee do
-        # Define routes for Employee within this block.
       end
     end
   end
